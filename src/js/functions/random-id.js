dce3bc537dfc998bf797e7c2b9e0e8d20dd71b9c
Cleibson Nenesio <cleibson.nenesio@hotmail.com>
2022-12-26 12:13:28 -0300

    Adicionada responsividade

--- src/js/functions/random-id.js
+++ src/js/functions/random-id.js
@@ -3,7 +3,7 @@ import { getCharaters } from "../services/get-characters.js"
 function getRandomId() {
     let randomCharactersIds = []
 
-    for(let x = 0; x < 9; x++) {
+    for(let x = 0; x < 7; x++) {
         randomCharactersIds.push(Math.floor(Math.random() * 826 + 1))
     }
 
